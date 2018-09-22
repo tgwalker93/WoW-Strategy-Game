a28aae3e1e4a71962fcec8315f89eb6ee54c627d
Tyler Walker <tgwalker93@gmail.com>
2018-09-22 08:52:25 -0700

    initial healthbar logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -902,3 +902,52 @@ chooseCharacter();
 
 });
 
+//HealthBar Logic
+var hitBtn = $('button.damage'),
+reset = $('button.reset'),
+hBar = $('.health-bar'),
+bar = hBar.find('.bar'),
+hit = hBar.find('.hit');
+
+hitBtn.on("click", function(){
+var total = hBar.data('total'),
+  value = hBar.data('value');
+
+if (value < 0) {
+	  log("you dead, reset");
+return;
+}
+// max damage is essentially quarter of max life
+var damage = Math.floor(Math.random()*total);
+// damage = 100;
+var newValue = value - damage;
+// calculate the percentage of the total width
+var barWidth = (newValue / total) * 100;
+var hitWidth = (damage / value) * 100 + "%";
+
+// show hit bar and set the width
+hit.css('width', hitWidth);
+hBar.data('value', newValue);
+
+setTimeout(function(){
+hit.css({'width': '0'});
+bar.css('width', barWidth + "%");
+}, 500);
+//bar.css('width', total - value);
+
+log(value, damage, hitWidth);
+
+if( value < 0){
+log("DEAD");
+}
+});
+
+reset.on('click', function(e){
+hBar.data('value', hBar.data('total'));
+
+hit.css({'width': '0'});
+
+  bar.css('width', '100%');
+  log("resetting health to 1000");
+});
+
